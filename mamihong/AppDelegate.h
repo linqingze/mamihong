5e6bb6a17e5514e4911bd3d95458381f1bb791d0
linqingze <13356584@qq.com>
2016-03-11 16:19:30 +0800

    init

---
+++ mamihong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mamihong
+//
+//  Created by mac on 16/3/11.
+//  Copyright © 2016年 XingChenZhongShi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+  
+
+@end
+
